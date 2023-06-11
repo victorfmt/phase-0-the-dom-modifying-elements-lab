370d51b9faeffca3da14ca0cb8a3ae209363bc46
Victor Torres <victor.fmt@gmail.com>
2023-06-11 18:55:41 -0400

    submission

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your code here!
+const main = document.getElementById("main");
+
+main.remove();
+
+const newHeader = document.createElement("h1");
+
+newHeader.id = 'victory';
+
+newHeader.textContent = "Victory is the champion";
+console.log(h1.textContent);
+
+    
